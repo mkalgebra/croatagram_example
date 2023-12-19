471ff25fa0e73b6d758e97a067140007ccf1d313
Marijo Kis <marijo.kis@q.agency>
2023-12-19 19:59:49 +0100

    chore: app skelet

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import "./App.css";
+import "./App.scss";
 import Post from "./components/Post/Post";
 
 function App() {
+  const posts = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
   return (
-    <div>
-      <h1>Croatagram</h1>
-      <h2>Feed</h2>
-      <div>
-        <Post />
+    <div className="c-app">
+      <h1 className="c-app__title">Croatagram</h1>
+      <h2 className="c-app__subtitle">Feed</h2>
+      <div className="c-app__content">
+        {posts.map((post) => (
+          <Post />
+        ))}
       </div>
     </div>
   );
